885c5ef1ebf36f968f96312db22a5cb15af8e510
tomcruzana <tomcruzana@ymail.com>
2022-09-23 09:05:10 -0700

    fixed design alignment

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,15 +1,23 @@
 const ctx = document.getElementById("myChart");
 const myChart = new Chart(ctx, {
-  type: "line",
+  type: "bar",
   data: {
-    labels: ["Red", "Red", "Red", "Red", "Red", "Red", "Red"],
+    labels: ["Red", "Blue", "Yellow"],
     datasets: [
       {
-        label: "My First Dataset",
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        tension: 0.1,
+        label: "# of Votes",
+        data: [12, 19, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
       },
     ],
   },
@@ -19,6 +27,5 @@ const myChart = new Chart(ctx, {
         beginAtZero: true,
       },
     },
-    responsive: true,
   },
 });
