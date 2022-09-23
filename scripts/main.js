87995d2f87f637551ea75936be2a02eb8a0eca56
tomcruzana <tomcruzana@ymail.com>
2022-09-22 23:36:42 -0700

    added some more things

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,58 @@ const totalDeaths = document.querySelector(".total-deaths");
 const totalRecovered = document.querySelector(".total-recovered");
 
 /******************************************* global variable**/
-let countriesNames = [];
+let countriesSlug = [];
+let coutryStats = [];
+
+/******************************************* global functions**/
+async function findCountry(countryName) {
+  console.log("Log: findCountry executed");
+  getStatusStatistics(countryName, options)
+    .then((response) => {
+      // get the last case from the status arrays
+      const confirmed = response.confirmed;
+      const deaths = response.deaths;
+      const recovered = response.recovered;
+      const lastConfirmed = confirmed[confirmed.length - 1].Cases;
+      const lastDeaths = deaths[deaths.length - 1].Cases;
+      const lastRecovered = recovered[recovered.length - 1].Cases;
+
+      // chart js
+      const ctx = document.getElementById("myChart");
+
+      const myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Confirmed", "Deaths", "Recovered"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [lastConfirmed, lastDeaths, lastRecovered],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+    })
+    .catch((err) => console.error(err));
+}
 
 /******************************************* events listeners**/
 // page load
@@ -28,15 +79,22 @@ window.addEventListener("load", (event) => {
     .then((response) => {
       response.forEach((e) => {
         console.log(e);
-        countriesNames.push(String(e.Country).toLowerCase());
+        countriesSlug.push(String(e.Slug).toLowerCase());
       });
     })
     .catch((err) => console.error(err));
 
-  // fetch statistics summary of each country
-  getStatistics(options)
+  // on page load fetch global statistics summary
+  getGlobalStatistics(options)
     .then((summary) => {
       console.log(summary);
+      // error handling
+      // if a message is detected in the api, exec alert msg
+      if (String(summary.Message).length > 0) {
+        alert(summary.Message + ". Please try again later.");
+        return;
+      }
+
       lastUpdated.textContent = String(summary.Global.Date).substring(0, 10);
       newConfirmed.textContent = summary.Global.NewConfirmed;
       newDeaths.textContent = summary.Global.NewDeaths;
@@ -50,7 +108,7 @@ window.addEventListener("load", (event) => {
 
 // input change
 input.addEventListener("input", (e) => {
-  let filteredProducts;
+  let filteredQuery;
   let query = "";
 
   // clear result-set each changes to avoid duplication
@@ -60,10 +118,10 @@ input.addEventListener("input", (e) => {
   query = String(input.value).toLowerCase();
 
   // filter relevant data as per search query
-  filteredProducts = countriesNames.filter((q) => q.startsWith(query));
+  filteredQuery = countriesSlug.filter((q) => q.startsWith(query));
 
   // show result-set
-  filteredProducts.forEach(
+  filteredQuery.forEach(
     (e) =>
       (inputSuggestions.innerHTML += `<div class="link"><h5>${e}</h5></div>`)
   );
@@ -74,6 +132,7 @@ input.addEventListener("input", (e) => {
 
 // event delegation for county links
 // assign target to input value
+// & load statistics to chart nad key stats
 inputSuggestions.addEventListener("click", function (e) {
   let country;
   if (e.target.tagName === "DIV") {
@@ -81,8 +140,10 @@ inputSuggestions.addEventListener("click", function (e) {
     const temp = String(e.target.innerHTML).replace("<h5>", "");
     country = temp.replace("</h5>", "");
     input.value = country;
+    findCountry(input.value);
   } else if (e.target.tagName === "H5") {
     input.value = e.target.innerHTML;
+    findCountry(input.value);
   }
 });
 
@@ -101,10 +162,35 @@ async function getCountries(options) {
   return countries;
 }
 
-// get statistics
-async function getStatistics(options) {
+// get global statistics
+async function getGlobalStatistics(options) {
   const response = await fetch(`https://api.covid19api.com/summary`, options);
   const summary = await response.json();
 
   return summary;
+}
+
+// get all status statistics
+async function getStatusStatistics(countrySlug, options) {
+  const responseConfirmed = await fetch(
+    `https://api.covid19api.com/total/country/${countrySlug}/status/confirmed`,
+    options
+  );
+
+  const responseDeaths = await fetch(
+    `https://api.covid19api.com/total/country/${countrySlug}/status/deaths`,
+    options
+  );
+
+  const responseRecovered = await fetch(
+    `https://api.covid19api.com/total/country/${countrySlug}/status/recovered`,
+    options
+  );
+
+  const confirmed = await responseConfirmed.json();
+  const deaths = await responseDeaths.json();
+  const recovered = await responseRecovered.json();
+
+  // return all cases as an object!!!
+  return { confirmed, deaths, recovered };
 }
